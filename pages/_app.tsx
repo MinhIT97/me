262253345876bcee4adb0be731625b009788496e
MinhIT97 <minh06081997@gmail.com>
2021-06-19 23:17:23 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
-import 'tailwindcss/tailwind.css'
+import "../styles/tailwind.scss";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
