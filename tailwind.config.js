e43fcb3b492d67923d4c7207d243e7627853546d
MinhIT97 <minh06081997@gmail.com>
2021-06-22 21:32:36 +0700

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./src/**/**/*.tsx" , "./pages/*.tsx" , "./pages/*.js" , "./component/*.js"],
+  purge: ["./src/**/**/*.tsx" , "./pages/*.tsx" , "./pages/*.js" , "./component/*.js" , "./component/*.jsx"],
   theme: {
     // good to know: add default line height https://tailwindcss.com/docs/font-size#app
     extend: {
