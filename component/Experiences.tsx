23a7849b6138619b3680dd5a33f768817b96fde9
kunchi <nguyenvanminh03@vccorp.vn>
2024-04-10 23:04:55 +0700

    update

--- component/Experiences.tsx
+++ component/Experiences.tsx
@@ -48,7 +48,7 @@ function Experiences() {
                                                 </h4>
                                             </div>
                                             <div className="text-customTurquoise-100 my-2">
-                                                11/2019 - 9/2020
+                                                11/2019 - 12/2020
                                             </div>
                                             <div>
                                                 {trans.experiences.responsibility} :
@@ -66,29 +66,7 @@ function Experiences() {
 
                                         </div>
                                     </div>
-                                    <div className="md:flex bg-gray-500 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-                                        <div className="pt-6 md:p-8 text-center md:text-left ">
-                                            <div>
-                                                <h4 className="text-xl">
-                                                    Software developer
-                                                    <a href="" className="text-customTurquoise-100 italic ml-2 font-semibold">Hiwin</a>
-                                                </h4>
-                                            </div>
-                                            <div className="text-customTurquoise-100 my-2">
-                                                10-2020 - 12/2020
-                                            </div>
-                                            <div>
-                                                {trans.experiences.responsibility} :
-                                                <ul className="list-disc ml-8">
-                                                    <li>Mobile app developer (React native)</li>
-                                                    <li>Parse api response</li>
-                                                </ul>
-                                            </div>
-                                            <div className="mt-3">
-                                                {trans.experiences.technology}: React native , redux.
-                                            </div>
-                                        </div>
-                                    </div>
+                                    
                                     <div className="md:flex bg-gray-500 rounded-xl p-8 md:p-0 dark:bg-slate-800">
                                         <div className="pt-6 md:p-8 text-center md:text-left ">
                                             <div>
