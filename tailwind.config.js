f6f5457c2f6ec2cfd41eb01dee057e05f874140b
kunchi <minh06081997@gmail.com>
2024-05-27 23:45:50 +0700

    image skill

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: ["./src/**/**/*.tsx", "./pages/*.tsx", "./pages/*.js", "./component/*.js", "./component/*.jsx", "./component/*.tsx"],
+    purge: ["./src/**/**/*.tsx", "./pages/*.tsx", "./pages/*.js", "./component/*.js", "./component/*.jsx", "./component/*.tsx", './component/*/*.tsx'],
     darkMode: 'class',
     theme: {
         // good to know: add default line height https://tailwindcss.com/docs/font-size#app
